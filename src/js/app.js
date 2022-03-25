be6ee3338ec92799e9ef7ea6d0f52fe88c6578e1
jedens5000 <mrjonedens@gmail.com>
2022-03-25 22:19:47 +0000

    Final commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,26 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-window.onload = function() {
-  let who = ['The dog','My grandma','His turtle','My bird'];
-  let action = ['ate','peed','crushed','broke'];
-  let what = ['my homework', 'the keys', 'the car'];
-  let when = ['before the class','right on time','this morning'];
-
-
-
-
-
-
-  function excusesForWork() {
-    let who1 = who[Math.floor(Math.random() * who.length)];
-    let action1 = action[Math.floor(Math.random() * action.length)];
-    let what1 = what[Math.floor(Math.random() * what.length)];
-    let when1 = when[Math.floor(Math.random() * when.length)];
-    
-    document.getElementById("excuse").innerHTML =
-    who1 + " " + action1 + " " + what1 + " " + when1;
-  }
-
-    document.getElementById("myBtn").addEventListener("click", excusesForWork);
-
-    console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#myBtn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  })
 };
 
+let generateExcuse = () => {
+  let who = ['The dog','My grandma','My turtle','My bird'];
+  let action = ['ate','peed on','crushed','broke'];
+  let what = ['my homework', 'the keys', 'the car'];
+  let when = ['before the class','last night','this morning'];
 
-// Moved from line 5 & 6////////////////////////////////
-// import "./assets/img/rigo-baby.jpg";
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return who[whoIndex] +" "+ action[actIndex] +" "+ what[whatIndex] +" "+ when[whenIndex] +".";  
+};
+document.querySelector("#myBtn").addEventListener("click", generateExcuse);
+
 // import "./assets/img/4geeks.ico";
